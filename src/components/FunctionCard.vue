5ac1fa0884fd3b13e031b8513f9b13492d6b2ee0
Bread <125883002+stefll@users.noreply.github.com>
2023-11-14 18:21:32 +0100

    feat: landingpage done (#4)
    * feat: landingpage done
    
    * fix: unused files deleted, consisten naming
    
    * fix: Working Routerview/Routerlinks, sticky navbar
    
    fix: rebased master in feature/dsgvo-imprint

---
+++ src/components/FunctionCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template >
+  <div class="flex flex-col items-center bg-primary rounded-[46px] pt-6 mt-6 p-2 pb-3">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <br>
+    <h2 class="font-nunito text-3xl text-text-black font-bold -mb-6 -mt-4 ">
+      <slot name="subheading"></slot>
+    </h2>
+    <br>
+    <p class="font-nunito-sans font-semibold text-text-black text-xl px-12 mt-1 mb-4 text-center">
+      <slot name="text"></slot>
+    </p>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import LandingPageView from "@/views/LandingPageView.vue";
+
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('../views/UserDashboardView.vue')
-    },
-    {
-      path: '/datenschutz',
-      name: 'datenschutz',
-      component: () => import('../views/DataProtectionView.vue')
-    },
-    {
-      path: '/impressum',
-      name: 'impressum',
-      component: () => import('../views/ImprintView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: LandingPageView
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: () => import('../views/UserDashboardView.vue')
+        },
+        {
+            path: '/datenschutz',
+            name: 'datenschutz',
+            component: () => import('../views/DataProtectionView.vue')
+        },
+        {
+            path: '/impressum',
+            name: 'impressum',
+            component: () => import('../views/ImprintView.vue')
+        }
+    ]
 })
 
 export default router
