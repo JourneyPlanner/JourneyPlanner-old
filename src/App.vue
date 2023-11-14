50f9d211dc67abaa22ad85110c99ae6d427286cb
Severin Rosner <einsevspielt@gmail.com>
2023-11-14 14:19:29 +0100

    wip: started dashboard

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import './style.css';
 
 <template>
   <header>
+    <!--
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -15,8 +16,13 @@ import './style.css';
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+
+        <RouterLink to="/test">Test</RouterLink>
+        <RouterLink to="/dashboard">Dashboard</RouterLink>
       </nav>
+
     </div>
+    -->
   </header>
 
   <RouterView />
---
+++ src/views/UserDashboardView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import HomeMarkerIcon from "../components/icons/IconHomeMarker.vue";
+import SettingsIcon from "../components/icons/IconSettings.vue";
+import UserDashboardJourneyItem from "../components/UserDashboardJourneyItem.vue";
+</script>
+
+<template>
+  <header class="flex flex-row">
+    <HomeMarkerIcon/>
+    <h1 class="font-nunito font-semibold text-4xl">Deine Reisen</h1>
+
+    <!-- Button noch, aber warten auf button folder -->
+
+    <!-- besser noch zu nem komponenten machen -->
+    <SettingsIcon />
+  </header>
+
+  <body>
+  <div id="journeys" class="grid grid-cols-4 gap-20">
+    <UserDashboardJourneyItem>
+      <template #journey-name>Stockholm</template>
+      <template #date-range>02.08. - 12.09.</template>
+    </UserDashboardJourneyItem>
+
+    <UserDashboardJourneyItem>
+      <template #journey-name>Stockholm</template>
+      <template #date-range>02.08. - 12.09.</template>
+    </UserDashboardJourneyItem>
+
+    <UserDashboardJourneyItem>
+      <template #journey-name>Stockholm</template>
+      <template #date-range>02.08. - 12.09.</template>
+    </UserDashboardJourneyItem>
+
+    <UserDashboardJourneyItem>
+      <template #journey-name>Stockholm</template>
+      <template #date-range>02.08. - 12.09.</template>
+    </UserDashboardJourneyItem>
+  </div>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
