a26fb674ee624aae1d70bdf3d21fbcfbc3ba79b1
Severin Rosner <einsevspielt@gmail.com>
2023-11-15 01:19:04 +0100

    fix: formatted and added jsdoc in src/components/buttons/BackButton.vue

--- src/components/buttons/BackButton.vue
+++ src/components/buttons/BackButton.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+/** check if there is a history to go back to **/
 function hasHistory() {
   return window.history.length > 2
 }
@@ -7,18 +8,8 @@ function hasHistory() {
 
 <template>
   <button @click="hasHistory() ? $router.back() : $router.push('/')"
-          class="
-          bg-call-to-action
-          border border-call-to-action rounded-[48px]
-          text-text-black font-nunito-sans font-semibold
-          text-base
-
-          sm:text-base sm:font-bold
-          md:text-xl
-          "
-
-  >
-    <!-- mx-8 my-4 -->
+          class=" bg-call-to-action border border-call-to-action rounded-[48px] text-text-black
+          font-nunito-sans font-semibold text-base sm:text-base sm:font-bold md:text-xl">
     Zurück
   </button>
 </template>
