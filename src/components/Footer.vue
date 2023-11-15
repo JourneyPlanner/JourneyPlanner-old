f9032a6caf684f028df62335ea29be806a912107
Severin Rosner <einsevspielt@gmail.com>
2023-11-15 18:30:42 +0100

    fix: fixed imprint resizing and improved footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,10 +4,10 @@
 
 <template>
   <div class="flex justify-center items-center">
-    <div class="bg-primary rounded-[68px] font-nunito font-light text-base px-4 py-2 mt-4 mb-2">
-      <RouterLink to="impressum" class="mr-3">Impressum</RouterLink>
-      <a href="mailto:contact@journeyplanner.io" class="font-semibold mr-3">contact@journeyplanner.io</a>
-      <RouterLink to="datenschutz" class="mr-3">Datenschutz</RouterLink>
+    <div class="bg-primary rounded-[68px] font-nunito font-light text-base py-2 mt-4 mb-2">
+      <RouterLink to="impressum" class="mr-1 ml-2 sm:ml-5 sm:mr-3">Impressum</RouterLink>
+      <a href="mailto:contact@journeyplanner.io" class="font-semibold mr-1 sm:mr-3">contact@journeyplanner.io</a>
+      <RouterLink to="datenschutz" class="mr-2 sm:mr-5">Datenschutz</RouterLink>
     </div>
   </div>
 </template>
