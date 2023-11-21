19d486b25ceaf3f803095468720d1bd7c9c6353f
Ixpiria <98148157+Ixpiria@users.noreply.github.com>
2023-11-21 16:07:54 +0100

    fix: style changed to be more like in the Prototype (#26)
    * fix: style changed to be more like in the Prototype
    
    * fix: prettier
    
    ---------
    
    Co-authored-by: Stefi <smanastirska@gmail.com>

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -63,47 +63,53 @@ export default {
 </script>
 <template>
   <div class="relative">
-    <div class="about bg-background flex">
+    <div class="bg-background flex">
       <div id="firsHalf" class="xl:w-1/2 md:w-2/3 sm:w-[100%] items-center justify-center flex flex-col">
         <BackToHomeButton class="absolute top-[2%] left-[2%]"/>
-        <div class="form xl:w-1/2 md:w-[80%] sm:w-[80%]">
+        <div class="xl:w-1/2 md:w-[80%] sm:w-[80%]">
           <h1 class="xl:text-3xl md:text-3xl sm:text-3xl pl-6.1538em font-nunito pt-[15%]">Registrierung</h1>
-          <form class="bg-primary rounded-2xl xl:w-[25vw] md:w-[50vw] sm:w-[75vw]">
-            <div class="inside items-center justify-center flex flex-col">
-              <h2 class=" text-left text-xl font-nunito font-semibold">Benutzername</h2>
-              <input v-model="v$.username.$model" class="font-nunito text-xl " placeholder="Benutzername eingeben">
+          <form class="bg-primary rounded-2xl xl:w-[25vw] md:w-[50vw] sm:w-[75vw] ">
+            <div class="inside flex flex-col pl-8 ">
+              <h2 class="col-start-1 text-xl font-nunito font-semibold">Benutzername</h2>
+              <input v-model="v$.username.$model"
+                     class="font-nunito text-xl border rounded border-none focus:outline-none focus:ring-2 focus:ring-call-to-action pl-1"
+                     placeholder="Benutzername eingeben">
               <p v-if="v$.username.$error" class="text-delete text-base font-nunito">Benutzername ist verpflichtend</p>
               <h2 class="text-xl font-nunito font-semibold">E-Mail</h2>
-              <input v-model="v$.contact.email.$model" class="font-nunito text-xl" placeholder="E-Mail eingeben">
+              <input v-model="v$.contact.email.$model"
+                     class="font-nunito text-xl border rounded border-none focus:outline-none focus:ring-2 focus:ring-call-to-action pl-1"
+                     placeholder="E-Mail eingeben">
               <p v-if="v$.contact.email.$error" class="text-delete text-base font-nunito">Nicht das richtige Format</p>
               <h2 class="text-xl font-nunito font-semibold">Passwort</h2>
-              <input v-model=v$.password.$model type="password" class="font-nunito text-xl"
+              <input v-model=v$.password.$model type="password"
+                     class="font-nunito text-xl border rounded border-none focus:outline-none focus:ring-2 focus:ring-call-to-action pl-1"
                      placeholder="Passwort eingeben">
-              <p v-if="v$.password.$error" class="text-delete text-base font-nunito">Muss zumindest 6 Character
+              <p v-if="v$.password.$error" class="text-delete text-base font-nunito">Muss zumindest 6 Zeichen
                 enthalten</p>
               <h2 class="text-xl font-nunito font-semibold">Passwort wiederholen</h2>
-              <input v-model=v$.passwordRepeat.$model type="password" class="font-nunito text-xl"
+              <input v-model=v$.passwordRepeat.$model type="password"
+                     class="font-nunito text-xl border rounded border-none focus:outline-none focus:ring-2 focus:ring-call-to-action pl-1"
                      placeholder="Passwort eingeben">
               <p v-if="v$.passwordRepeat.$error" class="text-delete text-base font-nunito">Nicht ident zu Passwort</p>
               <div class="check">
                 <label class="font-nunito text-base">
                   <input type="checkbox" v-model="v$.accepted.$model">
                   Hiermit akzeptiere ich die
-                  <RouterLink to="datenschutz">
+                  <RouterLink to="datenschutz" class="underline underline-offset-2 ml-4 lg:ml-0">
                     Datenschutzerklärung
                   </RouterLink>
                 </label>
               </div>
-              <div class="items-center justify-center flex p-3">
-                <button :disabled="v$.$invalid" @mouseover="v$.$touch()" type="button"
-                        class="registerButton bg-call-to-action rounded-3xl font-nunito text-xl font-bold p-1.5"
-                        @click="signUp">Registrieren
-                </button>
-                <RouterLink class="font-nunito text-base font-bold px-2 break-after-all" to="login">Schon registriert?
-                </RouterLink>
-              </div>
               <p v-if="v$.accepted.$error" class="text-delete text-base font-nunito">Bitte akzeptieren Sie die
                 Datenschutzerklärung</p>
+              <div class="my-3">
+                <button :disabled="v$.$invalid" @mouseover="v$.$touch()" type="button"
+                        class="registerButton bg-call-to-action rounded-3xl font-nunito text-xl font-bold p-1 px-2"
+                        @click="signUp">Registrieren
+                </button>
+                <RouterLink class="font-nunito text-base font-bold px-2 break-after-all underline underline-offset-2 lg:ml-4" to="login">Schon registriert?
+                </RouterLink>
+              </div>
             </div>
           </form>
         </div>
@@ -113,11 +119,13 @@ export default {
         <FliegerIllustration class="h-[85vh] ml-[1%]"/>
       </div>
     </div>
-      <Footer class="mt-14"/>
+    <div class="xl:w-1/2 md:w-2/3 sm:w-[100%] items-center justify-center flex ">
+      <Footer class="bottom-0 w-full md:fixed"/>
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 input:not([type="checkbox"]) {
   @apply flex w-[90%]
 }
