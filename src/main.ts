70e7dcccc848ef8c556080894323c4e3693c4bab
Ixpiria <roman.krebs@gmx.at>
2023-11-22 11:40:13 +0100

    feature: LoginView

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Button from "primevue/button"
+import Dialog from "primevue/dialog";
+import 'primevue/resources/themes/lara-light-green/theme.css'
 
 import PrimeVue from 'primevue/config';
 import Button from "primevue/button"
@@ -17,6 +21,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('Dialog', Dialog);
 
 app.use(PrimeVue);
 app.component('Button', Button);
