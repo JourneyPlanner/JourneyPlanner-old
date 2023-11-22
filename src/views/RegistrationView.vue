74bb8791fef5b0441f17e72c21dc869d33e3a7c1
Ixpiria <roman.krebs@gmx.at>
2023-11-22 12:28:27 +0100

    fix: fixed redirect URL

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -94,7 +94,7 @@ export default {
               <p v-if="v$.passwordRepeat.$error" class="text-delete text-base font-nunito">Nicht ident zu Passwort</p>
               <div class="check">
                 <label class="font-nunito text-base">
-                  <input type="checkbox" v-model="v$.accepted.$model">
+                  <input type="checkbox" class="p-3" v-model="v$.accepted.$model">
                   Hiermit akzeptiere ich die
                   <RouterLink :to="{ name: 'datenschutz'}" target="_blank" class="underline underline-offset-2 ml-4 lg:ml-0">
                     Datenschutzerklärung
