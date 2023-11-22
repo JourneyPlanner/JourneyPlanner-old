ae77b6f920bbf411782865159aa325df6171087c
Severin Rosner <einsevspielt@gmail.com>
2023-11-22 19:24:05 +0100

    wip: rls for fetching journeys

--- database/row-level-security.sql
+++ database/row-level-security.sql
@@ -1,9 +1,5 @@
-alter table journey
-    enable row level security;
-
-create policy "Allow create for authenticated users" on journey
-    for insert to authenticated with check (true);
-
 create policy "Allow select for user when member of journey" on journey
     for select using ((pk_journey_uuid = (select user_is_in.pk_journey_uuid from user_is_in where (user_is_in.pk_journey_uuid = auth.uid()))));
 
+create policy "Allow select for user" on user_is_in
+    for select using (auth.uid() = pk_user_uuid);
