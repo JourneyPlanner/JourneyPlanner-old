7214e979751bb544171e3a1c8736d20ef522e891
Mathematti <77683178+Mathematti@users.noreply.github.com>
2023-12-19 08:13:45 +0100

    Feature: User settings (#67)
    * feat: Account settings design
    
    * feat: Finish settings page including functionality

---
+++ src/components/illustrations/SettingsIllustration.vue
@@ -0,0 +1,116 @@
+<template>
+  <svg height="733" viewBox="0 0 791 733" width="791" xmlns="http://www.w3.org/2000/svg">
+    <g id="Gruppe_90" data-name="Gruppe 90" transform="translate(-1291 -369)">
+      <ellipse id="Ellipse_73" cx="364" cy="353" data-name="Ellipse 73" fill="#bad9e7" rx="364"
+               ry="353" transform="translate(1354 396)"/>
+      <g id="Ellipse_45" data-name="Ellipse 45" fill="none" stroke="#d8eaf3" stroke-width="4"
+         transform="translate(1291 369)">
+        <ellipse cx="367.5" cy="356.5" rx="367.5" ry="356.5" stroke="none"/>
+        <ellipse cx="367.5" cy="356.5" fill="none" rx="365.5" ry="354.5"/>
+      </g>
+      <g id="Gruppe_73" data-name="Gruppe 73" transform="translate(1223.605 182.681)">
+        <path id="Pfad_251" d="M372.782,698.111l-2.1-.041a487.41,487.41,0,0,1,7.46-69.655c9.088-49.263,24.2-81.616,44.915-96.161l1.207,1.72C375.87,567.95,372.806,696.812,372.782,698.111Z"
+              data-name="Pfad 251"
+              fill="#2f2e41" transform="translate(-175.278 65.987)"/>
+        <path id="Pfad_252" d="M397.782,693.112l-2.1-.041c.045-2.327,1.359-57.179,22.952-72.339l1.207,1.72C399.116,637,397.792,692.553,397.782,693.112Z"
+              data-name="Pfad 252"
+              fill="#2f2e41" transform="translate(-174.008 70.482)"/>
+        <circle id="Ellipse_67" cx="10.508" cy="10.508" data-name="Ellipse 67" fill="#f8d351"
+                r="10.508" transform="translate(250.431 579.136)"/>
+        <circle id="Ellipse_68" cx="10.508" cy="10.508" data-name="Ellipse 68" fill="#f06543"
+                r="10.508" transform="translate(245.228 669.505)"/>
+        <path id="Pfad_253" d="M393.693,541.12c1.975,12.614-3.172,23.9-3.172,23.9s-8.349-9.169-10.324-21.783,3.172-23.9,3.172-23.9S391.718,528.506,393.693,541.12Z"
+              data-name="Pfad 253"
+              fill="#2f2e41" transform="translate(-174.817 65.331)"/>
+        <path id="Pfad_254" d="M426.87,569.949c-12.077,4.143-24.083,1.04-24.083,1.04s7.574-9.819,19.651-13.961,24.083-1.04,24.083-1.04S438.947,565.807,426.87,569.949Z"
+              data-name="Pfad 254"
+              fill="#2f2e41" transform="translate(-173.647 67.136)"/>
+        <path id="Pfad_255" d="M426.891,645.9a32.717,32.717,0,0,1-16.88.729,29.815,29.815,0,0,1,30.654-10.515A32.716,32.716,0,0,1,426.891,645.9Z"
+              data-name="Pfad 255"
+              fill="#2f2e41" transform="translate(-173.28 71.218)"/>
+        <path id="Pfad_256" d="M607.656,470.028H593.849l-6.569-53.26h20.379Z" data-name="Pfad 256"
+              fill="#9e616a" transform="translate(70.581 280.289)"/>
+        <path id="Pfad_257" d="M835.676,690.58l-44.523,0v-.563a17.33,17.33,0,0,1,17.328-17.329h27.2Z"
+              data-name="Pfad 257"
+              fill="#2f2e41" transform="translate(-153.916 73.121)"/>
+        <path id="Pfad_258" d="M526.555,470.028H512.747l-6.569-53.26h20.379Z" data-name="Pfad 258"
+              fill="#9e616a" transform="translate(66.461 280.289)"/>
+        <path id="Pfad_259" d="M754.574,690.58l-44.523,0v-.563a17.33,17.33,0,0,1,17.328-17.329h27.2Z"
+              data-name="Pfad 259"
+              fill="#2f2e41" transform="translate(-158.037 73.121)"/>
+        <path id="Pfad_260" d="M719.176,393.472l-18.687-3.143-6.037,10.577L629.483,418.93l.176.975a11.507,11.507,0,0,0-14.532-2.565,13.72,13.72,0,0,0-6.258,14.5c1.123,5.813,5.65,10.111,11.093,10.533s10.485-3.135,12.353-8.716l78.679-17.86Z"
+              data-name="Pfad 260"
+              fill="#9e616a" transform="translate(-160.606 58.777)"/>
+        <path id="Pfad_261" d="M100.271,43.474a12.458,12.458,0,0,0-4.6.884l.382-.665L11.524,0,0,18.63,87.884,57.642a12.5,12.5,0,1,0,12.387-14.168Z"
+              data-name="Pfad 261"
+              fill="#9e616a" transform="translate(630.55 385.011) rotate(3)"/>
+        <circle id="Ellipse_69" cx="37.343" cy="37.343" data-name="Ellipse 69" fill="#2f2e41"
+                r="37.343" transform="translate(536.19 356.312) rotate(-80.783)"/>
+        <path id="Pfad_51" d="M0,26.7l27.95.262L24.488,0,4.527,1.028Z" data-name="Pfad 51"
+              fill="#9f616a" transform="matrix(0.985, -0.174, 0.174, 0.985, 564.435, 350.252)"/>
+        <circle id="Ellipse_70" cx="23.823" cy="23.823" data-name="Ellipse 70" fill="#a0616a"
+                r="23.823" transform="translate(555.211 315.476)"/>
+        <ellipse id="Ellipse_71" cx="25.754" cy="15.452" data-name="Ellipse 71" fill="#2f2e41" rx="25.754"
+                 ry="15.452" transform="translate(553.279 300.024)"/>
+        <circle id="Ellipse_72" cx="15.452" cy="15.452" data-name="Ellipse 72" fill="#2f2e41"
+                r="15.452" transform="translate(565.739 270.638)"/>
+        <path id="Pfad_262" d="M719.07,224.977a15.454,15.454,0,0,1,13.842-15.369,15.619,15.619,0,0,0-1.609-.084,15.452,15.452,0,0,0,0,30.9,15.625,15.625,0,0,0,1.61-.084,15.454,15.454,0,0,1-13.843-15.369Z"
+              data-name="Pfad 262"
+              fill="#2f2e41" transform="translate(-157.923 53.794)"/>
+        <path id="Pfad_263" d="M724.277,337.964l1.921-18.469s26.066-17.178,34.925-7.025l52.948,90.593s32.914,11.7,31.751,56.384l-1.574,215.982-37.322,4.091L784.262,511.128,763.786,685.589l-43.451-1.36L724.244,569.3l20.719-111.414-.2-36.97-9.125-14.92s-16.715-6.715-17.183-25.692l-.366-26.675Z"
+              data-name="Pfad 263"
+              fill="#2f2e41" transform="translate(-157.628 54.658)"/>
+        <path id="Pfad_264" d="M749.988,317.558l.514-8.658s78.844,20.777,72.589,35.632-17.983,19.546-17.983,19.546L759.761,346.1Z"
+              data-name="Pfad 264"
+              fill="#2f2e41" transform="translate(-156.008 54.64)"/>
+        <path id="Pfad_265" d="M732.394,337.947l-5.933-6.326s-47.32,66.4-33.01,73.818,26.325,3.524,26.325,3.524l23.357-42.827Z"
+              data-name="Pfad 265"
+              fill="#2f2e41" transform="translate(-159.017 55.794)"/>
+        <path id="Pfad_266" d="M22.91,183.575l274.136-59.27L274.136,0,0,59.27Z" data-name="Pfad 266"
+              fill="#fff" transform="matrix(1, 0.017, -0.017, 1, 455.958, 427.411)"/>
+        <path id="Pfad_267" d="M300.172,128.357,23.55,188.435,0,60.078,276.622,0ZM26.531,183.576l269.176-58.461L273.642,4.859,4.467,63.32Z"
+              data-name="Pfad 267"
+              fill="#e4e4e4" transform="matrix(1, 0.017, -0.017, 1, 455.558, 424.954)"/>
+        <path id="Pfad_271" d="M863.86,690.578H224.352a1.074,1.074,0,0,1,0-2.1H863.86a1.074,1.074,0,0,1,0,2.1Z"
+              data-name="Pfad 271"
+              fill="#cacaca" transform="translate(-32.712 73.924)"/>
+        <path id="Pfad_313" d="M4.818,0A4.669,4.669,0,0,0,0,4.5a4.669,4.669,0,0,0,4.818,4.5h82.27A4.669,4.669,0,0,0,91.9,4.5,4.669,4.669,0,0,0,87.087,0Z"
+              data-name="Pfad 313"
+              fill="#f8d351" transform="matrix(0.978, -0.208, 0.208, 0.978, 484.89, 500.985)"/>
+        <path id="Pfad_315" d="M2.842,0C1.272,0,0,1.732,0,3.869S1.272,7.738,2.842,7.738H51.375c1.57,0,2.842-1.732,2.842-3.869S52.944,0,51.375,0Z"
+              data-name="Pfad 315"
+              fill="#e6e6e6" transform="matrix(0.978, -0.208, 0.208, 0.978, 495.57, 526.921)"/>
+        <path id="Pfad_317" d="M2.839,0C1.271,0,0,1.657,0,3.7S1.271,7.4,2.839,7.4H51.324c1.568,0,2.839-1.657,2.839-3.7S52.892,0,51.324,0Z"
+              data-name="Pfad 317"
+              fill="#e6e6e6" transform="matrix(0.978, -0.208, 0.208, 0.978, 503.516, 556.343)"/>
+        <path id="Pfad_319" d="M2.531,0C1.133,0,0,1.518,0,3.391S1.133,6.783,2.531,6.783h43.22c1.4,0,2.531-1.518,2.531-3.391S47.149,0,45.751,0Z"
+              data-name="Pfad 319"
+              fill="#e6e6e6" transform="matrix(0.978, -0.208, 0.208, 0.978, 621.666, 499.241)"/>
+        <g id="Rechteck_301" data-name="Rechteck 301" fill="none"
+           stroke="#707070" stroke-width="0.5" transform="matrix(0.978, -0.208, 0.208, 0.978, 490.178, 522.908)">
+          <rect height="16.813" rx="6" stroke="none" width="86.232"/>
+          <rect fill="none" height="16.313" rx="5.75" width="85.732" x="0.25" y="0.25"/>
+        </g>
+        <g id="Rechteck_297" data-name="Rechteck 297" fill="none"
+           stroke="#707070" stroke-width="1" transform="matrix(0.978, -0.208, 0.208, 0.978, 480.924, 494.792)">
+          <rect height="22.067" rx="8" stroke="none" width="235.38"/>
+          <rect fill="none" height="21.067" rx="7.5" width="234.38" x="0.5" y="0.5"/>
+        </g>
+        <g id="Rechteck_298" data-name="Rechteck 298" fill="none"
+           stroke="#707070" stroke-width="0.5" transform="matrix(0.978, -0.208, 0.208, 0.978, 614.175, 495.587)">
+          <rect height="16.813" rx="6" stroke="none" width="86.232"/>
+          <rect fill="none" height="16.313" rx="5.75" width="85.732" x="0.25" y="0.25"/>
+        </g>
+        <rect id="Rechteck_299" data-name="Rechteck 299" fill="#f8d351" height="16.813" rx="6"
+              transform="matrix(0.978, -0.208, 0.208, 0.978, 503.84, 578.601)" width="56.81"/>
+        <g id="Rechteck_300" data-name="Rechteck 300" fill="none"
+           stroke="#707070" stroke-width="0.5" transform="matrix(0.978, -0.208, 0.208, 0.978, 497.536, 552.331)">
+          <rect height="16.813" rx="6" stroke="none" width="86.232"/>
+          <rect fill="none" height="16.313" rx="5.75" width="85.732" x="0.25" y="0.25"/>
+        </g>
+        <rect id="Rechteck_302" data-name="Rechteck 302" fill="#f06543" height="16.813" rx="6"
+              transform="matrix(0.978, -0.208, 0.208, 0.978, 653.055, 549.272)" width="56.81"/>
+      </g>
+    </g>
+  </svg>
+</template>
