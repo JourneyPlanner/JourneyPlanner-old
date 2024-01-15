3ba7d3a6ecb131e200f8a42420e5725d55685929
Severin <75267822+EinSev@users.noreply.github.com>
2024-01-15 07:03:14 +0100

    feat: kinda responsive dashboard (#136)
    * wip: responsive dashboard
    
    * wip: smartphone responsive dashboard
    
    * feat: kinda responsive dashboard

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex justify-center items-center overflow-x-hidden">
     <div class="bg-primary md:bg-secondary lg:bg-primary rounded-[68px] font-nunito font-light text-base py-2 mt-4 mb-2">
-      <RouterLink to="impressum" class="mr-1 ml-2 sm:ml-5 sm:mr-3 hover:opacity-80 hover:underline">Impressum</RouterLink>
+      <RouterLink to="/impressum" class="mr-1 ml-2 sm:ml-5 sm:mr-3 hover:opacity-80 hover:underline">Impressum</RouterLink>
       <a href="mailto:contact@journeyplanner.io" class="font-semibold mr-1 sm:mr-3 hover:opacity-80 hover:underline">contact@journeyplanner.io</a>
-      <RouterLink to="datenschutz" class="mr-2 sm:mr-5 hover:opacity-80 hover:underline">Datenschutz</RouterLink>
+      <RouterLink to="/datenschutz" class="mr-2 sm:mr-5 hover:opacity-80 hover:underline">Datenschutz</RouterLink>
     </div>
   </div>
 </template>
--- src/views/UserDashboardView.vue
+++ src/views/UserDashboardView.vue
@@ -13,40 +13,42 @@ const AsyncDashboard = defineAsyncComponent(() =>
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <header class="flex flex-row justify-between mb-4">
-      <div class="flex flex-row ml-10 mt-3">
-        <HomeMarkerIcon class="mt-3.5 ml-7 mr-5"/>
-        <h1 class="font-nunito font-medium text-4xl text-text-black">Deine Reisen</h1>
+    <header class="flex flex-col sm:flex-row justify-between ml-5 lg:ml-16">
+      <div class="flex flex-row">
+        <HomeMarkerIcon class="mt-3.5 mr-5"/>
+        <h1 class="font-nunito font-medium text-2xl sm:text-3xl lg:text-4xl mt-4 lg:mt-0 text-text-black">Deine Reisen</h1>
       </div>
 
-      <div class="flex flex-row mr-20">
-        <NewJourneyButton class="border-2 border-call-to-action hover:opacity-80 shadow-md" v-tooltip.bottom="{
+      <div class="flex flex-row h-24 justify-between sm:mt-4">
+        <NewJourneyButton class="border-2 border-call-to-action hover:opacity-80 shadow-md"
+                          v-tooltip.bottom="{
                value: 'Neue Reise erstellen',
-                 style:{
+               style:{
                    width: '30vw'
-                 }}"/>
+               }}"/>
 
-        <RouterLink to="einstellungen" v-tooltip.left="{
+        <RouterLink to="/einstellungen" v-tooltip.left="{
                value: 'Einstellungen',
                  style:{
                    width: '30vw'
                  }}">
-          <SettingsIcon class="mt-9 ml-5 w-14 hover:opacity-80"/>
+
+          <SettingsIcon class="mt-3 lg:mt-5 mx-5 w-10 lg:w-14 hover:opacity-80 lg:mr-20 mb-4"/>
         </RouterLink>
       </div>
     </header>
 
     <main>
-    <Suspense>
-      <template #default>
-        <AsyncDashboard/>
-      </template>
-      <template #fallback>
-        <div class="flex text-center justify-center">
-          <p class="text-3xl font-medium font-nunito text-text-black">Lade...</p>
-        </div>
-      </template>
-    </Suspense>
+      <Suspense>
+        <template #default>
+          <AsyncDashboard/>
+        </template>
+        <template #fallback>
+          <div class="flex text-center justify-center">
+            <p class="text-3xl font-medium font-nunito text-text-black">Lade...</p>
+          </div>
+        </template>
+      </Suspense>
     </main>
 
     <Footer class="mt-auto"/>
